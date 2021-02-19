54adb8d5fe68ba34449c1247ae738b44ae62f090
James Yoo <jimboyoo@gmail.com>
2021-02-18 19:46:16 -0800

    Styling UI forms, homepage SemanticUI

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import { Container, Header, Segment, Image, Button } from 'semantic-ui-react'
 
 export default function HomePage() {
    return (
-      <Container style={{marginTop: '7em'}}>
-         <h1>Eventlify Home</h1>
-         <h3>Go to <Link to='/activities'>Events </Link></h3>
-      </Container>
+      <Segment inverted textAlign='center' vertical className='masthead'>
+         <Container text>
+            <Header as='h1' inverted>
+               <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
+               Eventlify
+            </Header>
+            <Header as='h2' inverted content='Welcome to Eventlify' />
+            <Button as={Link} to='/activities' size='huge' inverted>Take me to Events!</Button>
+         </Container>
+      </Segment>
    )
 }
